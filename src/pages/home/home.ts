98dc3369cd5831aef9cd90cefd300c6e9c16a0a0
josuevargassosa <josuevargass@hotmail.com>
2019-01-05 03:19:04 -0500

    [UPDATE] Correction In GetItem Home.ts
    correcion de error al devolver el item buscado

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -12,6 +12,7 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
       this.initializeItems();
   }
+
   initializeItems() {
     this.items = [
       'Fiestas',
@@ -21,4 +22,20 @@ export class HomePage {
 
     ];
   }
+
+  getItems(ev) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // set val to the value of the ev target
+    var val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.items = this.items.filter((item) => {
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
+  
 }
