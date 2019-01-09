55c6a166eed60170ef96161d20b974140e37f53e
Siavichay7 <39249644+Siavichay7@users.noreply.github.com>
2019-01-09 00:18:35 -0500

    [UPDATE]: CHANGE IN ADD EVENT
    cambios en el formulario de registro de eventos, añadiendo iconos y validación del boton de registrar

--- src/pages/add-event/add-event.ts
+++ src/pages/add-event/add-event.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import * as firebase from 'firebase';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 /**
  * Generated class for the AddEventPage page.
@@ -21,22 +22,77 @@ export class AddEventPage {
     hour: string = '';
     ubication: string = '';
     date: string = '';
+    description: string = '';
+    final_date: string = '';
+    final_hour: string = '';
+    value: string = '';
     today = Date.now();
     register = [];
+    myForm: FormGroup;
+    public orderForm:any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
 
 
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, 
+    public formBuilder: FormBuilder) {
+
+      this.orderForm = this.formBuilder.group({
+        event_name: ['', Validators.required],
+      manager_name: ['', Validators.required],
+      category: ['', Validators.required],
+      ubication: ['', Validators.required],
+      date: ['', Validators.required],
+      final_date: ['', Validators.required],
+      hour: ['', Validators.required],
+      final_hour: ['', Validators.required],
+      description: ['', Validators.required],
+      value: ['', Validators.required],
+      
+      });
+      this.orderForm.reset()
+
+    //  this.myForm = this.createMyForm();
+
+   
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AddEventPage');
   }
 
+  saveData(){
+    console.log(this.myForm.value);
+  }
+  
   sendRegister(){
     var registerRef = firebase.database().ref().child("event_register");
-    registerRef.push({event_name: this.event_name, manager_name: this.manager_name, 
-      category: this.category, ubication: this.ubication, date: this.date, 
-    hour: this.hour});
+    registerRef.push({event_name: this.event_name, manager_name: this.manager_name,
+    category: this.category, hour: this.hour, ubication:this.ubication, date: this.date,
+  description: this.description, final_date: this.final_date, final_hour: this.final_hour,
+value: this.value});
+  }
+
+ 
+  
+    submit() {
+      //this.orderForm["event_name"].reset();
+      this.orderForm.reset()
+      console.log(this.orderForm.value);
+    }
+  private createMyForm(){
+    return this.formBuilder.group({
+      event_name: ['', Validators.required],
+      manager_name: ['', Validators.required],
+      category: ['', Validators.required],
+      ubication: ['', Validators.required],
+      date: ['', Validators.required],
+      final_date: ['', Validators.required],
+      hour: ['', Validators.required],
+      final_hour: ['', Validators.required],
+      description: ['', Validators.required],
+      value: ['', Validators.required],
+      
+    });
   }
 }
